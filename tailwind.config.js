69437ba088b3f192b0736eba3e51f5ca33bb2b91
AlphaThrone <haikalardzi@gmail.com>
2024-02-26 21:46:26 +0700

    update readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: 
+  {
+    relative: true,
+    files: [
+      './src/**/*.{html,js,jsx,tsx}',
+      '.{html,js,jsx,tsx}',
+      './index.html',
+    ],
+  },
   theme: {
     extend: {},
   },
