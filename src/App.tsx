4d21ab698d083006fe1a6e607a97c7ef95491a04
Christophorus Dharma Winata <13521009@std.stei.itb.ac.id>
2024-03-03 14:50:54 +0700

    fix: router imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import 'react-scroll'
-
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Catalog from './pages/Catalog';
+import Details from './pages/Details';
 
 function App() {
   return (
-    <div className='h-screen bg-stars bg-no-repeat bg-cover bg-local'>
-      <Catalog/>
-    </div>
-    // <BrowserRouter>
-    // <Routes>
-      
-    // </Routes>
-    // </BrowserRouter>
-    
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Catalog/>}/>
+            <Route path='catalog' element={<Catalog/>}/>
+            {/* <Route path='otherpages' element={<OtherPages/>}/> */
+            /* <Route path='otherpages' element={<OtherPages/>}/> */
+            /* <Route path='otherpages' element={<OtherPages/>}/> */
+            /* <Route path='otherpages' element={<OtherPages/>}/> */
+            /* <Route path='otherpages' element={<OtherPages/>}/> */
+            }
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
+    </div>    
   )
 }
 export default App
