1a27b41fc02e1dc348a55f32f21675681b59e813
Christophorus Dharma Winata <13521009@std.stei.itb.ac.id>
2024-03-03 17:52:19 +0700

    details page progress

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -6,18 +6,23 @@ const Details = () => {
             <div>
                 <Navbar/>
             </div>
-            <div> 
-                <h1 className="font-inter text-white text-left text-5xl font-bold">
-                    Imah Noong
-                </h1>
-                <p className="font-inter text-white text-left">
-                    Unassuming observatory with a collection of telescopes in simple surroundings.
-                </p>
-                {/* <div>
-                </div>*/}
+            <div className="px-12">
+                <div className="flex justify-between items-center"> 
+                    <div className="">
+                        <h1 className="font-inter text-white text-left text-5xl font-bold">
+                            Imah Noong
+                        </h1>
+                        <p className="font-inter text-white text-left">
+                            Unassuming observatory with a collection of telescopes in simple surroundings.
+                        </p>
+                    </div>
+                    <div className="max-w-[50vw]">
+                        <img className="" src="Imah Noong_ss.png"/>
+                    </div>
+                </div>
                 <div>
-                    <img className="" src="Imah Noong_ss.png"/>
-                </div> */
+                    
+                </div>
             </div>
         </div>
     );
