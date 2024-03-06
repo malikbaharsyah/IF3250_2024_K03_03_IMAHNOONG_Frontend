6d1885f6a63789013722726a76aa36d52dbc291a
AlphaThrone <haikalardzi@gmail.com>
2024-03-06 11:15:51 +0700

    feat: pesantiket1 80%

--- src/components/contents/CatalogContent.tsx
+++ src/components/contents/CatalogContent.tsx
@@ -33,7 +33,7 @@ import { CatalogContentProps } from "../../interfaces/planetarium";
                     <Link to={"#"}>
                         <button className="w-[207px] h-[69px] rounded-full bg-gradient-to-b hover:bg-opacity-40 hover:from-[#B5B5B5] hover:to-color-3 hover:transition from-color-2 to-color-3
                                         font-inter font-medium text-[24px]">
-                            See Details
+                            Kunjungi
                         </button>
                     </Link>
                 </div>
