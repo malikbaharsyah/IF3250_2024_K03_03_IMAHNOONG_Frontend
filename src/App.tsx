c896befc8b71baf75f657aa2490601f6ffc4eb62
Jauza Lathifah Annassalafi <13521030@std.stei.itb.ac.id>
2024-05-02 20:40:55 +0700

    fix: sidebar admin

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,21 @@ import HelpPage from './pages/help';
 import ListTiket from './pages/view_ticket'
 import Details from './pages/Details';
 import DetailPemesanan from './pages/DetailPemesanan';
+import Sidebar from './components/base/Sidebar';
 import Jadwal from './pages/Jadwal';
 import { DashboardAdmin } from './pages/DashboardAdmin';
 import RatingPage from './pages/Rating'
-import Pemesanan from './pages/Pemesanan';
+import PemesananAdmin from './pages/PemesananAdmin';
 import AddAcara from './pages/AddAcara';
 import AddKunjungan from './pages/AddKunjungan';
 import EditAcara from './pages/EditAcara';
 import EditKunjungan from './pages/EditKunjungan';
-import Profile from './pages/Profile';
+import Profile from './pages/EditProfil';
 import TicketRequest from './pages/TicketRequest';
+import SideNavBar from './components/base/SideNavBar';
+import AdminPage from './pages/AdminPage';
+import EditProfil from './pages/EditProfil';
+import Rating1 from './pages/Rating1';
 
 function App() {
   return (
@@ -29,10 +34,14 @@ function App() {
             <Route path='ticketrequest' element={<TicketRequest/>}/>
             <Route path='helppage' element={<HelpPage/>}/>
             <Route path='listtiket' element={<ListTiket/>}/>
-            <Route path='pemesanan' element={<Pemesanan/>}/>
             <Route path='details' element={<Details/>}/>
-            <Route path='detailpemesanan' element={<DetailPemesanan/>}/>
             <Route path='login' element={<LoginAdmin/>}/>
+            <Route path='rating1' element={<Rating1/>}/>
+        </Routes>
+        <Sidebar>
+          <Routes>
+            <Route path='pemesanan' element={<PemesananAdmin/>}/>
+            <Route path='detailpemesanan' element={<DetailPemesanan/>}/>
             <Route path='dashboard' element={<DashboardAdmin/>}/>
             <Route path='jadwal' element={<Jadwal/>}/>
             <Route path='addacara' element={<AddAcara/>}/>
@@ -40,8 +49,11 @@ function App() {
             <Route path='addkunjungan' element={<AddKunjungan/>}/> 
             <Route path='editkunjungan' element={<EditKunjungan/>}/>
             <Route path='rating' element={<RatingPage/>}/>
-            <Route path='profile' element={<Profile/>}/>
-        </Routes>
+            <Route path='profile' element={<EditProfil/>}/>
+            {/* <Route path='sidenavbar' element={<SideNavBar/>}/>
+            <Route path='adminpage' element={<AdminPage/>}/> */}
+          </Routes>
+        </Sidebar>
       </BrowserRouter>
     </div>    
   )
