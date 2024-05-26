8f13e14c8c3c7775ce74ac199c343de21a65a004
bintang433 <13521003@std.stei.itb.ac.id>
2024-05-26 23:12:03 +0700

    integrate: details

--- src/pages/view_ticket.tsx
+++ src/pages/view_ticket.tsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
-import Navbar from '../components/base/Navbar';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { ArrowForwardIosRounded, ArrowBackIosRounded } from '@mui/icons-material';
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/base/Navbar";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import {
+  ArrowForwardIosRounded,
+  ArrowBackIosRounded,
+} from "@mui/icons-material";
+import { Jadwal } from "../interfaces/detailsData";
 
 interface DateItem {
   date: Date;
 }
 
 const ViewTicket: React.FC = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get("id");
+
+  if (id === null) {
+    return <div></div>;
+  }
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [filteredDates, setFilteredDates] = useState<DateItem[]>([]);
   const [tickets, setTickets] = useState<DateItem[]>([]);
@@ -37,48 +47,115 @@ const ViewTicket: React.FC = () => {
 
   const handleSearch = () => {
     if (selectedDate) {
-      console.log('Search for ticket on', selectedDate);
+      console.log("Search for ticket on", selectedDate);
       // Perform search for tickets
     }
   };
 
-const handleDateItemClick = (date: Date) => {
-  setSelectedDate(date);
-};
+  const handleDateItemClick = (date: Date) => {
+    setSelectedDate(date);
+    handleDateChange(date);
+  };
 
-const TicketContent: React.FC<{ date: Date }> = ({ date }) => {
-  if (!selectedDate || date.getTime() !== selectedDate.getTime()) return null;
+  const TicketContent: React.FC<{ date: Date }> = ({ date }) => {
+    const [jadwalDefault, setJadwalDefault] = useState<Jadwal[]>([]);
+    console.log(date.toString())
+    useEffect(() => {
+      fetch("http://localhost:9000/api/jadwalDefault/listjadwal/"+id+"/"+date.toISOString().split('T')[0])
+        .then((response) => response.json())
+        .then((data) => {
+          setJadwalDefault(data);
+        })
 
-  const ticketForDate = tickets.find((ticket) => ticket.date.getTime() === date.getTime());
-  if (!ticketForDate) return null;
+        .catch((error) =>
+          console.error("Error fetching review data:", error)
+        );
+    }, []);
 
-  return (
-    <div className='flex space-x-4'>
-      <a href="#" className="flex flex-col justify-between bg-transparent border-white rounded-2xl w-[408px] h-[7vw] px-6 py-4">
-        <h5 className="font-bold text-[1.5vw] text-white">Nama Show</h5>
-        <div className='flex justify-between font-semibold text-[1vw]'>
-          <p className="text-white">{date.toLocaleDateString('id-ID', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' })}</p>
-          <p className="text-white">{date.toLocaleTimeString('id-ID', { hour: '2-digit', minute: '2-digit' })}</p>
+    if (!selectedDate || date.getTime() !== selectedDate.getTime()) return null;
+
+    const ticketForDate = tickets.find(
+      (ticket) => ticket.date.getTime() === date.getTime()
+    );
+    if (!ticketForDate) return null;
+
+    return (
+      jadwalDefault.map((item, index) => (
+        <div className="flex space-x-4" key={index}>
+          <a
+            href="#"
+            className="flex flex-col justify-between bg-transparent border-white rounded-2xl w-[408px] h-[7vw] px-6 py-4"
+          >
+            <h5 className="font-bold text-[1.5vw] text-white">{item.namaJadwal}</h5>
+            <div className="flex justify-between font-semibold text-[1vw]">
+              <p className="text-white">
+                {date.toLocaleDateString("id-ID", {
+                  weekday: "long",
+                  day: "numeric",
+                  month: "long",
+                  year: "numeric",
+                })}
+              </p>
+              <p className="text-white">
+                {date.toLocaleTimeString("id-ID", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </p>
+            </div>
+          </a>
+          <div className="container-harga flex flex-col items-center justify-between">
+            <div className="harga">
+              <p className="text-white text-[1.5vw] font-semibold">Rp. {item.hargaTiket}</p>
+            </div>
+            <div className="status">
+              <p className="text-white text-[1vw] font-medium">Tersedia</p>
+            </div>
+            <div className="beli-tiket">
+              <button className="text-white bg-gradient-to-r from-[#4F1395] to-[#2224A1] w-[10vw] rounded-full h-[3vw]">
+                Beli Tiket
+              </button>
+            </div>
+          </div>
         </div>
-      </a>
-      <div className='container-harga flex flex-col items-center justify-between'>
-        <div className='harga'>
-          <p className='text-white text-[1.5vw] font-semibold'>
-            Rp. 100.000
-          </p>
-        </div>
-        <div className='status'>
-          <p className='text-white text-[1vw] font-medium'>
-            Tersedia
-          </p>
-        </div>
-        <div className='beli-tiket'>
-          <button className='text-white bg-gradient-to-r from-[#4F1395] to-[#2224A1] w-[10vw] rounded-full h-[3vw]'>
-            Beli Tiket
-          </button>
-        </div>
-      </div>
-    </div>
+      ))
+        //<div className="flex space-x-4">
+        //</div></div> <a
+        //     href="#"
+        //     className="flex flex-col justify-between bg-transparent border-white rounded-2xl w-[408px] h-[7vw] px-6 py-4"
+        //   >
+        //     <h5 className="font-bold text-[1.5vw] text-white">Nama Show</h5>
+        //     <div className="flex justify-between font-semibold text-[1vw]">
+        //       <p className="text-white">
+        //         {date.toLocaleDateString("id-ID", {
+        //           weekday: "long",
+        //           day: "numeric",
+        //           month: "long",
+        //           year: "numeric",
+        //         })}
+        //       </p>
+        //       <p className="text-white">
+        //         {date.toLocaleTimeString("id-ID", {
+        //           hour: "2-digit",
+        //           minute: "2-digit",
+        //         })}
+        //       </p>
+        //     </div>
+        //   </a>
+        //   <div className="container-harga flex flex-col items-center justify-between">
+        //     <div className="harga">
+        //       <p className="text-white text-[1.5vw] font-semibold">Rp. 100.000</p>
+        //     </div>
+        //     <div className="status">
+        //       <p className="text-white text-[1vw] font-medium">Tersedia</p>
+        //     </div>
+        //     <div className="beli-tiket">
+        //       <button className="text-white bg-gradient-to-r from-[#4F1395] to-[#2224A1] w-[10vw] rounded-full h-[3vw]">
+        //         Beli Tiket
+        //       </button>
+        //     </div>
+        //   </div>
+        // </div>
     );
   };
 
@@ -119,8 +196,8 @@ const TicketContent: React.FC<{ date: Date }> = ({ date }) => {
     <div className="flex flex-col">
       <Navbar />
       <div className="flex flex-col items-center h-screen bg-stars bg-no-repeat bg-cover bg-local overflow-auto">
-        <p className='text-white font-bold mt-8 text-2xl'>Nama Planetarium</p>
-        <div className='my-8'>
+        <p className="text-white font-bold mt-8 text-2xl">Nama Planetarium</p>
+        <div className="my-8">
           <form className="flex flex-row py-4 px-6 space-x-4 bg-white bg-opacity-50 rounded-full items-center">
             <DatePicker
               selected={selectedDate}
@@ -132,27 +209,51 @@ const TicketContent: React.FC<{ date: Date }> = ({ date }) => {
             <button
               type="button"
               onClick={handleSearch}
-              className="text-white rounded-full py-2 px-6 bg-gradient-to-r from-[#4F1395] to-[#2224A1] ">
+              className="text-white rounded-full py-2 px-6 bg-gradient-to-r from-[#4F1395] to-[#2224A1] "
+            >
               Cari & Pesan Tiket
             </button>
-            </form>
+          </form>
+        </div>
+        <Slider {...settings} className="w-full px-20">
+          {filteredDates.map((dateItem, index) => (
+            <div
+              className="flex items-center justify-center my-5"
+              key={index}
+              onClick={() => handleDateItemClick(dateItem.date)}
+            >
+              <div
+                className={`flex flex-col mx-auto w-fit h-fit rounded-2xl cursor-pointer px-12 py-2 items-center justify-center space-y-2 ${
+                  dateItem.date === selectedDate
+                    ? "bg-white text-black"
+                    : "bg-gradient-to-r from-[#4F1395] to-[#2224A1] text-white"
+                }`}
+              >
+                <h3 className="font-bold text-xl">
+                  {dateItem.date.toLocaleDateString("id-ID", {
+                    weekday: "long",
+                  })}
+                </h3>
+                <h4 className="font-medium text-lg">
+                  {dateItem.date.toLocaleDateString("id-ID", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </h4>
+              </div>
             </div>
-            <Slider {...settings} className="w-full px-20">
-              {filteredDates.map((dateItem, index) => (
-                <div className="flex items-center justify-center my-5" key={index} onClick={() => handleDateItemClick(dateItem.date)}>
-                  <div className={`flex flex-col mx-auto w-fit h-fit rounded-2xl cursor-pointer px-12 py-2 items-center justify-center space-y-2 ${dateItem.date === selectedDate ? "bg-white text-black" : "bg-gradient-to-r from-[#4F1395] to-[#2224A1] text-white"}`}>
-                    <h3 className="font-bold text-xl">{dateItem.date.toLocaleDateString('id-ID', { weekday: 'long' })}</h3>
-                    <h4 className='font-medium text-lg'>{dateItem.date.toLocaleDateString('id-ID', { day: 'numeric', month: 'long', year: 'numeric' })}</h4>
-                  </div>
-                </div>
-              ))}
-            </Slider>
-            <div className="flex flex-col items-center mt-8">
-          <p className="text-white font-semibold text-xl my-4">Daftar Tiket Tersedia</p>
+          ))}
+        </Slider>
+        <div className="flex flex-col items-center mt-8">
+          <p className="text-white font-semibold text-xl my-4">
+            Daftar Tiket Tersedia
+          </p>
           <div className="grid grid-cols-2 gap-4 mt-4">
-            {selectedDate && tickets.map((item, index) => (
-              <TicketContent key={index} date={item.date} />
-            ))}
+            {selectedDate &&
+              tickets.map((item, index) => (
+                <TicketContent key={index} date={item.date} />
+              ))}
           </div>
         </div>
       </div>
