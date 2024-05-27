0bb2b0af35fd290eaf085b8221b7fba6f7d718cb
ardzihs <13521012@std.stei.itb.ac.id>
2024-05-27 15:22:46 +0700

    feat: integration of request ticket

--- src/pages/TicketRequest.tsx
+++ src/pages/TicketRequest.tsx
@@ -1,9 +1,14 @@
 import TicketRequestContent from "../components/contents/TicketRequestContent"
 
 const TicketRequest = () => {
+
+    const urlParams = new URLSearchParams(window.location.search);
+
+    const pid = urlParams.get('pid');
+
     return (
         <div className='h-screen bg-stars bg-no-repeat bg-cover bg-local overflow-x-hidden'>
-            <TicketRequestContent/>
+            <TicketRequestContent planetariumId = {pid!!}/>
         </div>
     )
 }
